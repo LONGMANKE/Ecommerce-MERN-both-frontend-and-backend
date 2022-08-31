cf85c7c43068cb7509ef1dd4c06a3779576feb70
LONGMANKE <mburunjoroge0@gmail.com>
2022-08-31 02:38:26 +0100

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
-import Header from "./component/layout/Header.js"
-import {BrowserRouter as Router} from "react-router-dom"
+import Header from "./component/layout/Header/Header.js"
+import Footer from "./component/layout/Footer/Footer.js"
+import { BrowserRouter as Router } from "react-router-dom"
+import WebFont from "webfontloader"
+import React from 'react';
 
-function App() {
-  return (
-    <Router><Header/></Router>
-    
+
+
+function App() { 
+
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"]
+      }
+  })
+  }, [])
+
+
+  return (  
+    <Router>
+      <Header />
+      <Footer/>
+      </Router>
+
   );
 }
 
