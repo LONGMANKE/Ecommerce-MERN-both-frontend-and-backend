c5aedcf2818baf7c7771a9ba41aed7a64d8d3b4b
LONGMANKE <mburunjoroge0@gmail.com>
2022-08-31 03:12:50 +0100

    footer css

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -17,14 +17,14 @@ const Footer = () => {
         <h1>ECOMMERCE.</h1>
         <p>High Quality is our first priority</p>
 
-        <p>Copyrights 2021 &copy; MeAbhiSingh</p>
+        <p>Copyrights 2021 &copy; MBURU</p>
       </div>
 
       <div className="rightFooter">
         <h4>Follow Us</h4>
-        <a href="http://instagram.com/meabhisingh">Instagram</a>
-        <a href="http://youtube.com/6packprogramemr">Youtube</a>
-        <a href="http://instagram.com/meabhisingh">Facebook</a>
+        <a href="http://instagram.com/m.b.u.r.u.u">Instagram</a>
+        <a href="https://youtube.com/channel/UCY4w6WS1XMPBqIEmPypifVQ">Youtube</a>
+        <a href="http://facebook.com/LONGMANKE">Facebook</a>
       </div>
     </footer>
   );
