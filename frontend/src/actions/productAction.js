8617d4cf6cdb8d54509599f1b5b16fc58721fb73
LONGMANKE <mburunjoroge0@gmail.com>
2022-09-01 22:25:56 +0100

    redux

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -2,4 +2,14 @@ import axios from "axios"
 import { ALL_PRODUCT_FAIL, ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, CLEAR_ERRORS } from "../constants/productConstants"
 
 
-export const
+export const getProduct = () => async (dispatch) =>{
+    try {
+        dispatch({type: ALL_PRODUCT_REQUEST});
+        const {data} = await axios.get 
+    } catch (error) {
+        dispatch({
+            tyoe: ALL_PRODUCT_FAIL,
+            payload:error.response.data.message,
+        })
+    }
+}
