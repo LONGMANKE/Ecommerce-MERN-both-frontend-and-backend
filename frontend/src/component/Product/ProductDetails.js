c516104c0b5964ed3e6ab55924a755a2ededd1c0
LONGMANKE <mburunjoroge0@gmail.com>
2022-09-14 18:26:57 +0100

    fucking error sorted

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -3,31 +3,32 @@ import Carousel from "react-material-ui-carousel"
 import "./ProductDetails.css"
 import { useSelector, useDispatch } from "react-redux"
 import { getProductDetails } from "../../actions/productAction"
+// import { useParams } from "react-router-dom";
 
 const ProductDetails = ({ match }) => {
   const dispatch = useDispatch();
 
-  const { product } = useSelector(
-    (state) => state.productDetails);
-
+  const { product } = useSelector((state) => state.productDetails);
   useEffect(() => {
 
     dispatch(getProductDetails(match.params.id));
   }, [dispatch, match.params.id]);
+  
+ 
 
   return (
     <Fragment>
       <div className='ProductDetails'>
       <div>
               <Carousel>
-                {product.images &&
-                  product.images.map((item, i) => 
+              {product.images && [product.images].map((item, i) => (
                     <img
                       className="CarouselImage"
-                      key={i}
+                      key={item.url}
                       src={item.url}
                       alt={`${i} Slide`}
-                    />
+                    />)
+                    
               )
             }
           </Carousel>
@@ -36,6 +37,29 @@ const ProductDetails = ({ match }) => {
       </div>
 
     </Fragment>
+    // <Fragment>
+    //   <div className="ProductDetails">
+    //     <div>
+    //       <Carousel>
+
+    //       {product && product.images &&
+    //           product.images.map((item, i) => (
+    //               <img
+    //                 className='CarouselImage'
+    //                 key={item.url}
+    //                 src={item.url}
+    //                 alt={`${i} Slide`} 
+    //               />
+    //           ))}
+
+    //           </Carousel>
+            
+            
+            
+          
+    //     </div>
+    //   </div>
+    // </Fragment>
 
 
 
