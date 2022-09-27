8dcdba4524254347d83a7fdfb128c7ae4962f47d
LONGMANKE <mburunjoroge0@gmail.com>
2022-09-27 02:15:35 +0100

    Stripe api

--- backend/app.js
+++ backend/app.js
@@ -13,11 +13,15 @@ app.use(fileUpload());
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
+
 
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
+app.use("/api/v1", order);
+
 
 
 
